e0d74143c04c1be4f53f3611129bc73b1dd9a875
Evandro Moresco <64448748+mor3sco@users.noreply.github.com>
2026-06-11 21:50:31 -0300

    Delete TaskFlow/backend/src/routes/middleware/config/prisma.js

--- TaskFlow/backend/src/routes/middleware/config/prisma.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const { PrismaClient } = require('@prisma/client');
-
-const prisma = new PrismaClient({
-  log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
-});
-
-module.exports = prisma;
